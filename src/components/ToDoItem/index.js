734ee985e6e5132bb8a2fcfbd353d9c504047db5
dohun <dohun31@naver.com>
2022-06-06 16:55:02 +0900

    Issue #12 refactor: state를 사용해 re-render마다 색 변경 방지

--- src/components/ToDoItem/index.js
+++ src/components/ToDoItem/index.js
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MdDone, MdDelete } from "react-icons/md";
 import { useTodoDispatch } from "../../TodoContext";
 import S from "./styled";
 import useRandomColor from "../../hooks/useRandomColor";
 
 function TodoItem({ id, done, text }) {
-    const [setColor, getRandomColor] = useRandomColor();
+    const [setColors, getRandomColor] = useRandomColor();
+    const [color, setColor] = useState("");
     const dispatch = useTodoDispatch();
     const onToggle = () => dispatch({ type: "TOGGLE", id });
     const onRemove = () => dispatch({ type: "REMOVE", id });
 
+    useEffect(() => {
+        setColor(getRandomColor());
+    }, []);
+
     return (
         <S.TodoItemBlock>
-            <S.CheckCircle done={done} onClick={onToggle} color={getRandomColor()}>
+            <S.CheckCircle done={done} onClick={onToggle} color={color}>
                 {done && <MdDone />}
             </S.CheckCircle>
             <S.Text done={done}>{text}</S.Text>
--- src/hooks/useRandomColor.js
+++ src/hooks/useRandomColor.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
 function useRandomColor() {
-    const [color, setColor] = useState(["#ffa500", "#fa8072", "#adff2f", "#7fffd4"]);
+    const [color, setColors] = useState(["#ffa500", "#fa8072", "#adff2f", "#7fffd4"]);
 
     const getRandomColor = () => {
         const randomNumber = Math.floor(Math.random() * color.length);
         return color[randomNumber];
     };
 
-    return [setColor, getRandomColor];
+    return [setColors, getRandomColor];
 }
 
 export default useRandomColor;
