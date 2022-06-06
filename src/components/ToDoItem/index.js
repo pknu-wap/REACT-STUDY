ff24ee4a175a8282249d23992dd5dcd15be6f6a0
dohun <dohun31@naver.com>
2022-06-06 16:26:47 +0900

    Issue #12 feat: TodoItem 색 랜덤하게 변경

--- src/components/ToDoItem/index.js
+++ src/components/ToDoItem/index.js
@@ -3,22 +3,28 @@ import { MdDone, MdDelete } from "react-icons/md";
 import { useTodoDispatch } from "../../TodoContext";
 import S from "./styled";
 
-function TodoItem({ id, done, text }) {
-  const dispatch = useTodoDispatch();
-  const onToggle = () => dispatch({ type: "TOGGLE", id });
-  const onRemove = () => dispatch({ type: "REMOVE", id });
+const getRandomColor = () => {
+    const colors = ["#ffa500", "#fa8072", "#adff2f", "#7fffd4"];
+    const randomNumber = Math.floor(Math.random() * 4);
+    return colors[randomNumber];
+};
 
-  return (
-    <S.TodoItemBlock>
-      <S.CheckCircle done={done} onClick={onToggle}>
-        {done && <MdDone />}
-      </S.CheckCircle>
-      <S.Text done={done}>{text}</S.Text>
-      <S.Remove onClick={onRemove}>
-        <MdDelete />
-      </S.Remove>
-    </S.TodoItemBlock>
-  );
+function TodoItem({ id, done, text }) {
+    const dispatch = useTodoDispatch();
+    const onToggle = () => dispatch({ type: "TOGGLE", id });
+    const onRemove = () => dispatch({ type: "REMOVE", id });
+
+    return (
+        <S.TodoItemBlock>
+            <S.CheckCircle done={done} onClick={onToggle} color={getRandomColor()}>
+                {done && <MdDone />}
+            </S.CheckCircle>
+            <S.Text done={done}>{text}</S.Text>
+            <S.Remove onClick={onRemove}>
+                <MdDelete />
+            </S.Remove>
+        </S.TodoItemBlock>
+    );
 }
 
 export default React.memo(TodoItem);
