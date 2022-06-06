84ef620efcd9b139a82b999556cefdc3c02a64fa
dohun <dohun31@naver.com>
2022-06-06 15:41:07 +0900

    Issue #11 refactor: component들 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { createGlobalStyle } from "styled-components";
-import TodoHead from "./components/TodoHead";
-import TodoTemplate from "./components/TodoTemplate";
-import TodoList from "./components/TodoList";
-import TodoCreate from "./components/TodoCreate";
+import TodoHead from "./components/TodoHead/TodoHead";
+import TodoTemplate from "./components/TodoTemplate/TodoTemplate";
+import TodoList from "./components/TodoList/TodoList";
+import TodoCreate from "./components/TodoCreate/TodoCreate";
 import { TodoProvider } from "./TodoContext";
 
 const GlobalStyle = createGlobalStyle`
