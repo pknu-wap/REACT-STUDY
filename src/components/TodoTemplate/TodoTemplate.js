a4c806171e6b30a8f0df761f3d8ef17bb4fcf4cb
dohun <dohun31@naver.com>
2022-06-06 15:38:59 +0900

    Issue #11 refactor: TodoTemplate styled, 로직 분리

---
+++ src/components/TodoTemplate/TodoTemplate.js
@@ -0,0 +1,8 @@
+import React from "react";
+import S from "./styled";
+
+function TodoTemplate({ children }) {
+    return <S.TodoTemplateBlock>{children}</S.TodoTemplateBlock>;
+}
+
+export default TodoTemplate;
