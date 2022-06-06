f66dd5078ce69ea06b3e97dcba589f7a8d7cd77d
dohun <dohun31@naver.com>
2022-06-06 16:57:29 +0900

    Issue #12 refactor: 변수 네이밍 변경

--- src/hooks/useRandomColor.js
+++ src/hooks/useRandomColor.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
 function useRandomColor() {
-    const [color, setColors] = useState(["#ffa500", "#fa8072", "#adff2f", "#7fffd4"]);
+    const [colors, setColors] = useState(["#ffa500", "#fa8072", "#adff2f", "#7fffd4"]);
 
     const getRandomColor = () => {
-        const randomNumber = Math.floor(Math.random() * color.length);
-        return color[randomNumber];
+        const randomNumber = Math.floor(Math.random() * colors.length);
+        return colors[randomNumber];
     };
 
     return [setColors, getRandomColor];
